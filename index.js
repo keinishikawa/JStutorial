13c6c67b4a02468ac4a9cf763fbdf7c9ddae2b13
keinishikawa <keinishikawa4@gmail.com>
2021-01-13 22:24:50 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let unko = "Hello World";
+// unko = "Hello World2";
+
+//定数
+const bigUnko = "He..Hell...Hello World!";
+
+// bigUnko = "Hello World2";
+let inoki = ["いーち","にー","さーん","だーーーーー"];
+// console.log(inoki[1]);
+
+
+// let index = 0;
+// while(index < inoki.length){
+//   console.log(inoki[index]);
+//   index++;
+// }
+
+const test = (arg) => {
+  let index = 0;
+while(index < arg){
+  console.log(inoki[index]);
+  index++;
+}
+
+};
+
+const unko2 = {
+  color: "pink",
+  size: "large",
+  purfume:"mint",
+  goToilet: () =>{
+    console.log("Hello World");
+  }
+};
+
+console.log(window.innerHeight);
+window.alert("unko");
+console.log(document.getElementsByTagName("button")[0]);
